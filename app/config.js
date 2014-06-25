24608ae416a504803740eeee98fa78d340385c85
Robert Williams <robert@increscent.org>
2014-06-25 13:56:29 -0600

    testing angular setup

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
-var scripturesApp = angular.module('scripturesApp', []);
+var scripturesApp = angular.module('scripturesApp', ['ng-route']);
 
 scripturesApp.config(function ($routeProvider) {
     $routProvider
