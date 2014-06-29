e51a0769c5cf3d66bca013dd1f8ae47ba34878dd
robert williams <robert@increscent.org>
2014-06-28 18:13:13 -0600

    added server functionality

--- app/factory.js
+++ app/factory.js
@@ -3,18 +3,12 @@ scripturesApp.factory('indexFactory', function() {
 	var index = localStorage.index;
 	if (localStorage.index) {
 		index = JSON.parse(localStorage.index);
-		console.log(index);
+		console.log(index.ot_kjv);
 	} else {
-		/*ajax_request('/index.json', '', function (data) {
-			index = JSON.parse(data);
-			console.log(index);
-			localStorage.index = JSON.stringify(index);
-		}); */
-		
 		var response = $http.get("/index.json");
 		response.success(function(data, status, headers, config) {
-			index = JSON.parse(data);
-			console.log(index);
+			index = data;
+			console.log(index.ot_kjv);
 			localStorage.index = JSON.stringify(index);
 		});
 		response.error(function(data, status, headers, config) {
