f049a77a6158337f65c0b120fa55c6f941460633
robert williams <robert@increscent.org>
2014-07-18 09:17:45 -0700

    reverted

--- app/app.js
+++ app/app.js
@@ -3,7 +3,4 @@ var scripturesApp = angular.module('scripturesApp', ['ngRoute']);
 scripturesApp.config(function ($routeProvider, $locationProvider) {
 	$routeProvider
 		.otherwise({redirectTo: '/works'});
-	
-	$locationProvider.html5Mode(true);
-	$locationProvider.hashPrefix('!');
 });
