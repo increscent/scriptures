3468eadcb61d6b9f44d40c76bf37e22c22011263
robert williams <robert@increscent.org>
2014-07-18 09:04:33 -0700

    html5mode

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 var scripturesApp = angular.module('scripturesApp', ['ngRoute']);
 
-scripturesApp.config(function ($routeProvider) {
+scripturesApp.config(function ($routeProvider, $locationProvider) {
 	$routeProvider
 		.otherwise({redirectTo: '/works'});
+	
+	$locationProvider.html5Mode(true);
 });
