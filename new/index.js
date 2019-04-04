69941a7f97c3604411d8eb6db8cca1b31fbdeb16
Robert Williams <robert@increscent.org>
2019-04-04 14:55:59 -0600

    making progress on scriptures

--- new/index.js
+++ new/index.js
@@ -5,70 +5,150 @@ const toc = require('./toc.js');
 
 const app = express();
 const port = 1435;
-const pageSize = 700; // characters per page
+const pageSize = 500; // characters per page
+
+app.get('/', (req, res) => render(res, 'bofm', '1-ne', '1'));
 
 app.get('/:work/:book/:chapter/:verse?', (req, res) =>
-    {
-        try {
-            // make sure the input matches an existing work/book/chapter
-            // this helps to guard against attacks
-            var work = req.params.work;
-            var book = req.params.book;
-            var chapter = parseInt(req.params.chapter);
-            var verse = req.params.verse ? parseInt(req.params.verse) : 1;
-            var bookData = toc[work].books[book];
-            if (chapter < 1 || chapter > bookData.chapters)
-                throw null;
-        } catch (e)
-        {
-            return res.send("Not found");
-        }
-
-//        console.log(Object.keys(toc[work].books));
-
-        fs.readFile(`./${work}/${book}/${book}${chapter}`, (err, data) => 
-            {
-                if (err)
-                    return res.send("File read error");
-
-                var chapterData = JSON.parse(data.toString());
-                var chapterView = fs.readFileSync('./views/chapter.html').toString();
-                var chapterVerses = chapterData.verses.map((v, i) => ({number: i+1, verse: v}));
-
-                if (verse > chapterVerses.length)
-                {
-                    var [nextBook, nextChapter] = getNextChapter(toc, work, book, chapter);
-                    return res.redirect(`/${work}/${nextBook}/${nextChapter}`);
-                }
-
-                var verses = [];
-                var limit = pageSize;
-                for (var i = verse-1; i < chapterVerses.length && limit > 0; i++)
-                {
-                    verses.push(chapterVerses[i]);
-                    limit -= chapterVerses[i].verse.length;
-                }
-
-                var nextVerse = verse + verses.length;
-                var nextBook = book;
-                var nextChapter = chapter;
-                if (nextVerse > chapterVerses.length)
-                {
-                    [nextBook, nextChapter] = getNextChapter(toc, work, book, chapter);
-                    nextVerse = 1;
-                }
-                var nextLink = `/${work}/${nextBook}/${nextChapter}/${nextVerse}`;
-
-                res.send(mustache.render(chapterView, {
-                    verses, 
-                    book: bookData.name,
-                    chapter,
-                    nextLink,
-                }));
-            });
-    });
+    render(res, req.params.work, req.params.book, req.params.chapter, req.params.verse));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+function render(res, work, book, chapter, verse)
+{
+    try {
+        // make sure the input matches an existing work/book/chapter
+        // this helps to guard against attacks
+        chapter = parseInt(chapter);
+        verse = verse ? parseInt(verse) : 1;
+        var bookData = toc[work].books[book];
+        if (chapter < 1 || chapter > bookData.chapters)
+            throw null;
+    } catch (e)
+    {
+        return res.send("Not found");
+    }
+
+    Promise.all([
+        getChapterVerses(work, book, chapter),
+        readFile('./views/chapter.html'),
+        getNextVerse(toc, work, book, chapter, verse),
+        getPrevVerse(toc, work, book, chapter, verse),
+    ])
+    .then(([chapterVerses, chapterView, next, prev]) =>
+        {
+            var [nextBook, nextChapter, nextVerse] = next;
+            var [prevBook, prevChapter, prevVerse] = prev;
+
+            if (verse > chapterVerses.length)
+            {
+                var [nextBook, nextChapter] = getNextChapter(toc, work, book, chapter);
+                return res.redirect(`/${work}/${nextBook}/${nextChapter}`);
+            }
+
+            var verses = chapterVerses
+                .map((x, i) => ({number: i+1, verse: x.verse}))
+                .slice(verse-1, 
+                    (nextVerse > verse) ? nextVerse-1 : chapterVerses.length);
+
+            var nextLink = `/${work}/${nextBook}/${nextChapter}/${nextVerse}`;
+            var prevLink = `/${work}/${prevBook}/${prevChapter}/${prevVerse}`;
+
+            res.send(mustache.render(chapterView, {
+                verses, 
+                book: bookData.name,
+                chapter,
+                nextLink,
+                prevLink,
+            }));
+        }
+    )
+    .catch(err =>
+        {
+            console.log(err);
+            res.send("There was an error.");
+        }
+    );
+}
+
+function getNextVerse(toc, work, book, chapter, verse)
+{
+    return getChapterVerses(work, book, chapter)
+    .then(chapterVerses =>
+        {
+            var limit = pageSize;
+            var verseIndex = verse-1;
+            var verses = 0;
+            while (verseIndex < chapterVerses.length && limit > 0)
+            {
+                limit -= chapterVerses[verseIndex].verse.length;
+                verses++;
+                verseIndex++;
+            }
+
+            var nextVerse = verse + verses;
+            var nextBook = book;
+            var nextChapter = chapter;
+            if (nextVerse > chapterVerses.length)
+            {
+                [nextBook, nextChapter] = getNextChapter(toc, work, book, chapter);
+                nextVerse = 1;
+            }
+
+            return [nextBook, nextChapter, nextVerse];
+        }
+    );
+}
+
+function getPrevVerse(toc, work, book, chapter, verse)
+{
+    if (verse <= 1)
+        [book, chapter] = getPrevChapter(toc, work, book, chapter);
+
+    return getChapterVerses(work, book, chapter)
+    .then(chapterVerses =>
+        {
+            var prevVerse; 
+
+            if (verse <= 1)
+            {
+                // find the last page in the chapter
+                var verseIndex = 0;
+                var verses;
+                while (verseIndex < chapterVerses.length)
+                {
+                    var limit = pageSize;
+                    verses = 0;
+                    while (verseIndex < chapterVerses.length && limit > 0)
+                    {
+                        limit -= chapterVerses[verseIndex].verse.length;
+                        verses++;
+                        verseIndex++;
+                    }
+                }
+
+                prevVerse = chapterVerses.length+1 - verses;
+            }
+            else
+            {
+                // find the previous page in the chapter
+                var limit = pageSize;
+                var verseIndex = verse-1-1;
+                var verses = 0;
+                while (verseIndex >= 0 && limit > 0)
+                {
+                    limit -= chapterVerses[verseIndex].verse.length;
+                    verses++;
+                    verseIndex--;
+                }
+
+                prevVerse = verse - verses;
+            }
+
+            return [book, chapter, prevVerse];
+        }
+    );
+}
 
 function getNextChapter(toc, work, book, chapter)
 {
@@ -96,12 +176,32 @@ function getPrevChapter(toc, work, book, chapter)
         var books = Object.keys(toc[work].books);
         var bookIndex = books.indexOf(book);
         bookIndex--;
-        if (bookIndex < 0)
-            return [books[books.length-1], 1];
-        else
-            return [books[bookIndex], 1];
+        var nextBook = (bookIndex < 0) ? books[books.length-1] : books[bookIndex];
+        return [nextBook, toc[work].books[nextBook].chapters];
     } else
     {
         return [book, chapter-1];
     }
+}
+
+function getChapterVerses(work, book, chapter)
+{
+    return readFile(`./${work}/${book}/${book}${chapter}`)
+    .then(fileData => JSON.parse(fileData).verses.map((v, i) => ({number: i+1, verse: v})));
+}
+
+function readFile(filename)
+{
+    return new Promise((resolve, reject) =>
+        {
+            fs.readFile(filename, (err, data) =>
+                {
+                    if (err)
+                        return reject(err);
+
+                    resolve(data.toString());
+                }
+            );
+        }
+    );
 }
