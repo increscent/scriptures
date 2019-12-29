2568154aa0dd647307b13646ce890d4b588b0a20
Robert Williams <robert@increscent.org>
2019-12-28 23:35:14 -0700

    Moved stuff around. Re-wrote part of backend

--- index.js
+++ index.js
@@ -1,1 +1,208 @@
-var index = {"ot_kjv": {"books": {"gen": {"abbr": "gen", "chapters": 50, "name": "Genesis"}, "ex": {"abbr": "ex", "chapters": 40, "name": "Exodus"}, "lev": {"abbr": "lev", "chapters": 27, "name": "Leviticus"}, "num": {"abbr": "num", "chapters": 36, "name": "Numbers"}, "deut": {"abbr": "deut", "chapters": 34, "name": "Deuteronomy"}, "josh": {"abbr": "josh", "chapters": 24, "name": "Joshua"}, "judg": {"abbr": "judg", "chapters": 21, "name": "Judges"}, "ruth": {"abbr": "ruth", "chapters": 4, "name": "Ruth"}, "1-sam": {"abbr": "1-sam", "chapters": 31, "name": "1 Samuel"}, "2-sam": {"abbr": "2-sam", "chapters": 24, "name": "2 Samuel"}, "1-kgs": {"abbr": "1-kgs", "chapters": 22, "name": "1 Kings"}, "2-kgs": {"abbr": "2-kgs", "chapters": 25, "name": "2 Kings"}, "1-chr": {"abbr": "1-chr", "chapters": 29, "name": "1 Chronicles"}, "2-chr": {"abbr": "2-chr", "chapters": 36, "name": "2 Chronicles"}, "ezra": {"abbr": "ezra", "chapters": 10, "name": "Ezra"}, "neh": {"abbr": "neh", "chapters": 13, "name": "Nehemiah"}, "esth": {"abbr": "esth", "chapters": 10, "name": "Esther"}, "job": {"abbr": "job", "chapters": 42, "name": "Job"}, "ps": {"abbr": "ps", "chapters": 150, "name": "Psalms"}, "prov": {"abbr": "prov", "chapters": 31, "name": "Proverbs"}, "eccl": {"abbr": "eccl", "chapters": 12, "name": "Ecclesiastes"}, "song": {"abbr": "song", "chapters": 8, "name": "Song Of Solomon"}, "isa": {"abbr": "isa", "chapters": 66, "name": "Isaiah"}, "jer": {"abbr": "jer", "chapters": 52, "name": "Jeremiah"}, "lam": {"abbr": "lam", "chapters": 5, "name": "Lamentations"}, "ezek": {"abbr": "ezek", "chapters": 48, "name": "Ezekiel"}, "dan": {"abbr": "dan", "chapters": 12, "name": "Daniel"}, "hosea": {"abbr": "hosea", "chapters": 14, "name": "Hosea"}, "joel": {"abbr": "joel", "chapters": 3, "name": "Joel"}, "amos": {"abbr": "amos", "chapters": 9, "name": "Amos"}, "obad": {"abbr": "obad", "chapters": 1, "name": "Obadiah"}, "jonah": {"abbr": "jonah", "chapters": 4, "name": "Jonah"}, "micah": {"abbr": "micah", "chapters": 7, "name": "Micah"}, "nahum": {"abbr": "nahum", "chapters": 3, "name": "Nahum"}, "hab": {"abbr": "hab", "chapters": 3, "name": "Habakkuk"}, "zeph": {"abbr": "zeph", "chapters": 3, "name": "Zephaniah"}, "hag": {"abbr": "hag", "chapters": 2, "name": "Haggai"}, "zech": {"abbr": "zech", "chapters": 14, "name": "Zechariah"}, "mal": {"abbr": "mal", "chapters": 4, "name": "Malachi"}}, "abbr": "ot_kjv", "name": "Old Testament (KJV)"}, "nt_kjv": {"books": {"matt": {"abbr": "matt", "chapters": 28, "name": "Matthew"}, "mark": {"abbr": "mark", "chapters": 16, "name": "Mark"}, "luke": {"abbr": "luke", "chapters": 24, "name": "Luke"}, "john": {"abbr": "john", "chapters": 21, "name": "John"}, "acts": {"abbr": "acts", "chapters": 28, "name": "Acts"}, "rom": {"abbr": "rom", "chapters": 16, "name": "Romans"}, "1-cor": {"abbr": "1-cor", "chapters": 16, "name": "1 Corinthians"}, "2-cor": {"abbr": "2-cor", "chapters": 13, "name": "2 Corinthians"}, "gal": {"abbr": "gal", "chapters": 6, "name": "Galatians"}, "eph": {"abbr": "eph", "chapters": 6, "name": "Ephesians"}, "philip": {"abbr": "philip", "chapters": 4, "name": "Philippians"}, "col": {"abbr": "col", "chapters": 4, "name": "Colossians"}, "1-thes": {"abbr": "1-thes", "chapters": 5, "name": "1 Thessalonians"}, "2-thes": {"abbr": "2-thes", "chapters": 3, "name": "2 Thessalonians"}, "1-tim": {"abbr": "1-tim", "chapters": 6, "name": "1 Timothy"}, "2-tim": {"abbr": "2-tim", "chapters": 4, "name": "2 Timothy"}, "titus": {"abbr": "titus", "chapters": 3, "name": "Titus"}, "philem": {"abbr": "philem", "chapters": 1, "name": "Philemon"}, "heb": {"abbr": "heb", "chapters": 13, "name": "Hebrews"}, "james": {"abbr": "james", "chapters": 5, "name": "James"}, "1-pet": {"abbr": "1-pet", "chapters": 5, "name": "1 Peter"}, "2-pet": {"abbr": "2-pet", "chapters": 3, "name": "2 Peter"}, "1-jn": {"abbr": "1-jn", "chapters": 5, "name": "1 John"}, "2-jn": {"abbr": "2-jn", "chapters": 1, "name": "2 John"}, "3-jn": {"abbr": "3-jn", "chapters": 1, "name": "3 John"}, "jude": {"abbr": "jude", "chapters": 1, "name": "Jude"}, "rev": {"abbr": "rev", "chapters": 22, "name": "Revelation"}}, "abbr": "nt_kjv", "name": "New Testament (KJV)"}, "bofm": {"books": {"bofm_introduction": {"abbr": "bofm_introduction", "chapters": 1, "name": "Introduction"}, "1-ne": {"abbr": "1-ne", "chapters": 22, "name": "First Nephi"}, "2-ne": {"abbr": "2-ne", "chapters": 33, "name": "Second Nephi"}, "jacob": {"abbr": "jacob", "chapters": 7, "name": "Jacob"}, "enos": {"abbr": "enos", "chapters": 1, "name": "Enos"}, "jarom": {"abbr": "jarom", "chapters": 1, "name": "Jarom"}, "omni": {"abbr": "omni", "chapters": 1, "name": "Omni"}, "w-of-m": {"abbr": "w-of-m", "chapters": 1, "name": "Words Of Mormon"}, "mosiah": {"abbr": "mosiah", "chapters": 29, "name": "Mosiah"}, "alma": {"abbr": "alma", "chapters": 63, "name": "Alma"}, "hel": {"abbr": "hel", "chapters": 16, "name": "Helaman"}, "3-ne": {"abbr": "3-ne", "chapters": 30, "name": "Third Nephi"}, "4-ne": {"abbr": "4-ne", "chapters": 1, "name": "Fourth Nephi"}, "morm": {"abbr": "morm", "chapters": 9, "name": "Mormon"}, "ether": {"abbr": "ether", "chapters": 15, "name": "Ether"}, "moro": {"abbr": "moro", "chapters": 10, "name": "Moroni"}}, "abbr": "bofm", "name": "Book of Mormon"}, "dc": {"books": {"dc_introduction": {"abbr": "dc_introduction", "chapters": 1, "name": "Explanatory Introduction"}, "section": {"abbr": "section", "chapters": 138, "name": "Sections"}, "od_001": {"abbr": "od_001", "chapters": 1, "name": "Official Declaration\u20141"}, "od_002": {"abbr": "od_002", "chapters": 1, "name": "Official Declaration\u20142"}}, "abbr": "dc", "name": "Doctrine & Covenants"}, "pgp": {"books": {"pgp_introduction": {"abbr": "pgp_introduction", "chapters": 1, "name": "Introductory Note"}, "moses": {"abbr": "moses", "chapters": 8, "name": "Moses"}, "abr": {"abbr": "abr", "chapters": 5, "name": "Abraham"}, "js-m_001": {"abbr": "js-m_001", "chapters": 1, "name": "Joseph Smith - Matthew"}, "a-of-f_001": {"abbr": "a-of-f_001", "chapters": 1, "name": "The Articles Of Faith"}}, "abbr": "pgp", "name": "Pearl of Great Price"}};
+const express = require('express');
+const fs = require('fs');
+const mustache = require('mustache');
+const cookieParser = require('cookie-parser');
+const TOC = require('./toc.js');
+
+const app = express();
+const port = 1435;
+const PAGE_SIZE = 600; // characters per page
+
+var FILES = {};
+
+app.use(cookieParser());
+
+app.get('/toc/:work/:book?', (req, res) =>
+    renderToc(res, req.params.work, req.params.book));
+
+app.get('/', (req, res) =>
+    {
+        if (req.cookies.lastVisited)
+            res.redirect(req.cookies.lastVisited);
+        else
+            renderPage(res, 'bofm', '1-ne', '1');
+    }
+);
+
+app.get('/:work/:book/:chapter/:verse?/:page?', (req, res) =>
+    renderPage(
+        res,
+        req.params.work,
+        req.params.book,
+        req.params.chapter,
+        req.params.verse,
+        req.params.page
+    ));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+function renderToc(res, work, book)
+{
+    try {
+        book = book || '1-ne';
+        var bookData = TOC[work].books[book];
+    } catch (e)
+    {
+        console.log(e);
+        return res.send("Not found");
+    }
+
+    let tocView = readFile('./views/toc.html')
+
+    var books = Object.values(TOC[work].books)
+        .map(x => ({...x, link: (x.chapters == 1) ? 
+            `/${work}/${x.abbr}/1` : `/toc/${work}/${x.abbr}`}));
+    var chapters = [...Array(TOC[work].books[book].chapters).keys()]
+        .map(x => ({chapter: x+1, book}));
+
+    res.send(mustache.render(tocView, {
+        work,
+        books,
+        chapters,
+    }));
+}
+
+function renderPage(res, work, book, chapter, verse, page)
+{
+    if (!TOC[work] || !TOC[work].books[book])
+        return res.send('Not found');
+
+    chapter = parseInt(chapter);
+    if (chapter < 1 || chapter > TOC[work].books[book].chapters)
+        return res.send('Not found');
+
+    let [pages, versePages] = getChapterPages(work, book, chapter, PAGE_SIZE);
+
+    if (verse == 'page') {
+        page = Math.max(1, Math.min(parseInt(page), pages.length));
+    } else {
+        page = versePages[verse] || 1;
+    }
+
+    let [nextWork, nextBook, nextChapter, nextPage] = getNext(
+        work,
+        book,
+        chapter,
+        page,
+        PAGE_SIZE
+    );
+    let [prevWork, prevBook, prevChapter, prevPage] = getPrev(
+        work,
+        book,
+        chapter,
+        page,
+        PAGE_SIZE
+    );
+
+    let chapterView = readFile('./views/chapter.html');
+
+    var nextLink = `/${work}/${nextBook}/${nextChapter}/page/${nextPage}`;
+    var prevLink = `/${work}/${prevBook}/${prevChapter}/page/${prevPage}`;
+
+    res.cookie('lastVisited', `/${work}/${book}/${chapter}/page/${page}`,
+        {
+            expire: new Date() + 1000*60*60*24*7*12,
+            maxAge: 1000*60*60*24*7*12
+        });
+
+    res.send(mustache.render(chapterView, {
+        content: pages[page-1], 
+        bookName: TOC[work].books[book].name,
+        work,
+        book,
+        chapter,
+        nextLink,
+        prevLink,
+    }));
+}
+
+function getNext(work, book, chapter, page, pageSize) {
+    let [pages, versePages] = getChapterPages(work, book, chapter, pageSize);
+
+    if (page < pages.length)
+        return [work, book, chapter, page+1];
+
+    if (chapter < TOC[work].books[book].chapters)
+        return [work, book, chapter+1, 1];
+
+    var books = Object.keys(TOC[work].books);
+    var bookIndex = books.indexOf(book)+1;
+    if (bookIndex < books.length)
+        return [work, books[bookIndex], 1, 1];
+ 
+    // Restart the work
+    return [work, books[0], 1, 1];
+}
+
+function getPrev(work, book, chapter, page, pageSize) {
+    let [pages, versePages] = getChapterPages(work, book, chapter, pageSize);
+
+    if (page > 1)
+        return [work, book, chapter, page-1];
+
+    if (chapter > 1) {
+        [pages, versePages] = getChapterPages(work, book, chapter-1, pageSize);
+        return [work, book, chapter-1, pages.length];
+    }
+
+    var books = Object.keys(TOC[work].books);
+    var bookIndex = books.indexOf(book)-1;
+    if (bookIndex >= 0) {
+        book = books[bookIndex];
+        chapter = TOC[work].books[book].chapters;
+        [pages, versePages] = getChapterPages(work, book, chapter, pageSize);
+        return [work, book, chapter, pages.length];
+    }
+ 
+    // End of the work
+    book = books[books.length-1];
+    chapter = TOC[work].books[book].chapters;
+    [pages, versePages] = getChapterPages(work, book, chapter, pageSize);
+    return [work, book, chapter, pages.length];
+}
+
+function getChapterVerses(work, book, chapter)
+{
+    return readFile(`./${work}/${book}/${book}${chapter}`, true).verses;
+}
+
+function getChapterPages(work, book, chapter, pageSize) {
+    let chapterVerses = getChapterVerses(work, book, chapter);
+    let pages = [];
+    let versePages = {};
+
+    let currentPage = '';
+    let currentLen = 0;
+    for (let i = 0; i < chapterVerses.length; i++) {
+        let verse = chapterVerses[i];
+
+        currentPage += ` <span style="color: #aaa">${i+1}</span> `;
+        versePages[(i+1).toString()] = pages.length + 1; // Pages are 1-indexed
+
+        if (currentLen + verse.length >= pageSize) {
+            let cutoffIndex = (verse.indexOf(' ', pageSize - currentLen) + 1) || verse.length;
+
+            currentPage += verse.substring(0, cutoffIndex);
+            pages.push(currentPage);
+
+            currentPage = verse.substring(cutoffIndex);
+            currentLen = currentPage.length;
+        } else {
+            currentPage += verse;
+            currentLen += verse.length;
+        }
+    }
+    pages.push(currentPage);
+
+    return [pages, versePages];
+}
+
+function readFile(filename, json)
+{
+    if (!FILES[filename]) {
+        let data = fs.readFileSync(filename).toString();
+        FILES[filename] = json ? JSON.parse(data) : data;
+    }
+
+    return FILES[filename];
+}
